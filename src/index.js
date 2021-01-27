bad0af34528e5103248bbd267c0193fc747beeae
arkadiibasovych <arkadiybasovich@gmai.com>
2021-01-28 00:38:01 +0200

    add templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import headertTemplates from './templates/header-templates';
+
+
+
+const headerRef = document.querySelector('.header-container-js');
+
+
+headerRef.insertAdjacentHTML('beforeend', headertTemplates.homeHeader);
