4f6cc21de5088e7850234163eb6a9fa1c3245f6c
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-02-05 14:08:24 +0200

    Видаляє непотрібні посилання

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,4 @@
 const refs = {
-  ulListMovie: document.querySelector('.list-movie-search-js'),
-  liFilmsGalleryItem: document.querySelector('.films-gallery-item'),
   headerSearchWarningShow() {
     return document.querySelector('#header-search-warning-show');
   },
